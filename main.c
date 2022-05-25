37a50374c5d1305c618635843147b42e53c7937a
kdanilovic <105845601+kdanilovic@users.noreply.github.com>
2022-05-25 23:24:22 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -4,22 +4,24 @@
 #include <time.h>
 #include <string.h>
 #include "funkcije.h"
+#include "struktura.h"
 
 
 
-typedef struct obaveze {
-	int br;
-	char obaveza[200];
-}OBAVEZE;
 
-static int BrojObaveze;
-static int brojac;
+char* datoteka = "to do list.txt";
+
+static int BrojObaveze=1;
+
+
+
+void naslov();
 int izbornik();
 void stvori_datoteku();
-void naslov();
 void dodaj();
+void* ucitaj();	
 void prikaz();
-
+void IzbrisiDatoteku();
 
 //Funkcije za kalendar
 void kalendar();
@@ -44,6 +46,7 @@ char* mjeseci[] =
 
 int main(void) {
 
+	
 	stvori_datoteku();
 	naslov();
 	izbornik();
@@ -59,6 +62,7 @@ void naslov() {
 
 int izbornik() {
 	int opcija;
+
 	do {
 		
 		printf("\n\t\t\t\t\t\t1. Dodaj obavezu\n\n");
@@ -66,12 +70,15 @@ int izbornik() {
 		printf("\t\t\t\t\t\t3. Uredi postojecu obavezu\n\n");
 		printf("\t\t\t\t\t\t4. Prikaz obaveza\n\n");
 		printf("\t\t\t\t\t\t5. Prikazi kalendar\n\n");
-		printf("\t\t\t\t\t\t6. Izadji iz programa\n\n");
+		printf("\t\t\t\t\t\t6. Izbrisi cijelu listu\n\n");
+		printf("\t\t\t\t\t\t7. Izadji iz programa\n\n");
 
 		printf("\nOdabir opcije: ");
 		scanf("%d", &opcija);
 		
 
+		static OBAVEZE* PoljeObaveza = NULL;
+		static OBAVEZE* TrazenaObaveza = NULL;
 
 		
 		switch (opcija) {
@@ -79,10 +86,10 @@ int izbornik() {
 			dodaj();
 			break;
 		case 2:
-			printf("\nIzbrisi obavezu broj:  ");
+			//printf("\nIzbrisi obavezu broj:  ");
 			break;
 		case 3:
-			printf("\nUredi postojecu obavezu broj: ");
+			//printf("\nUredi postojecu obavezu broj: ");
 			break;
 		case 4:
 			prikaz();
@@ -92,6 +99,9 @@ int izbornik() {
 			kalendar();
 			break;
 		case 6:
+			IzbrisiDatoteku();
+			break;
+		case 7:
 			system("cls");
 			printf("\nIzlazenje iz programa!");
 			exit(0);
@@ -100,68 +110,92 @@ int izbornik() {
 			break;
 		}
 
-	} while (opcija != 6);
+	} while (opcija != 7);
 	return opcija;
 
 }
 
 
 void stvori_datoteku() {
-	char* datoteka = "to do list.txt";
+	
 	FILE* mapa = NULL;
 	mapa = fopen(datoteka, "a");
 
 	if (mapa == NULL) {
 		perror("Problem: ");
+		exit(EXIT_FAILURE);
 	}
 	else {
 		printf("\nDatoteka napravljena");
 	}
 	
+	fclose(mapa);
 }
+
+
 
 void dodaj() {
 	system("cls");
 	
 	FILE* fp = NULL;
-	fp = fopen("to do list.txt","a");
+	fp = fopen(datoteka,"a");
 
 	if (fp == NULL) {
 		perror("dodavanje clanova error");
+		exit(EXIT_FAILURE);
 		
 	}
 	else {
 		printf("\nDATOTEKA ZA DODAVANJE CLANOVA UCITANA");
 	}
 
-	OBAVEZE* pok = NULL;
-	pok = (OBAVEZE*)malloc(sizeof(OBAVEZE));
-	if (pok == NULL) {
-		perror("Pokreska kod zauzimanja memorije za strukturu");
-	}
+	
+	
+	OBAVEZE temp = { 0 };
+
 	fread(&BrojObaveze, sizeof(int),1, fp);
-	printf("\nbroj obaveze: %d\n", BrojObaveze);
-	pok->br = BrojObaveze;
+	temp.br = BrojObaveze;
 	
 	printf("\nUnesite obavezu: ");
 	getchar();
-	scanf("%[^\n]",&pok->obaveza);
-	printf("%s",pok->obaveza);
+	scanf("%199[^\n]", temp.obaveza);
+	printf("%s", temp.obaveza);
 	
-	fprintf(fp, "%d. %s\n", pok->br, pok->obaveza);
+	fprintf(fp, "%d. %s\n\n", temp.br, temp.obaveza);
 	
-	system("pause");
+
 	BrojObaveze++;
-	system("cls");
+	
 	fclose(fp);
+	system("cls");
 }
  
+void* ucitaj() {
+	FILE* fpp = NULL;
+	fpp = fopen(datoteka,"r");
+	if (fpp == NULL) {
+		perror("Ucitavanje obaveza error");
+		exit(EXIT_FAILURE);
+	}
 
+	fread(&BrojObaveze, sizeof(int), 1, fpp);
+	printf("\nBroj obaveza unesenih: %d\n",BrojObaveze);
+	
+	OBAVEZE* PoljeObaveza = (OBAVEZE*)calloc(BrojObaveze, sizeof(OBAVEZE));
+	if (PoljeObaveza == NULL) {
+		perror("Problem kod zauzimanja memorije za polje obaveza");
+		exit(EXIT_FAILURE);
+	}
+
+	fread(PoljeObaveza,sizeof(OBAVEZE),BrojObaveze,fpp);
+	return PoljeObaveza;
+	fclose(fpp);
+}
 
 void prikaz() {
 	system("cls");
 	FILE* fp = NULL;
-	fp = fopen("to do list.txt", "r");
+	fp = fopen(datoteka, "r");
 
 	if (fp == NULL) {
 		perror("citanje clanova error");
@@ -182,6 +216,29 @@ void prikaz() {
 		system("cls");
 		fclose(fp);
 	}
+}
+
+void IzbrisiDatoteku() {
+	
+	char YesNo;
+	
+
+	do {
+		printf("\nJeste li sigurni da zelite izbrisati postojecu listu, te stvoriti novu? y/n: ");
+		scanf(" %c", &YesNo);
+	} while (YesNo != 'n' && YesNo != 'N' && YesNo != 'y' && YesNo != 'Y');
+
+
+	if (YesNo == 'y' || YesNo == 'Y') {
+		FILE* fp = NULL;
+		fp = fopen(datoteka, "w");
+
+		if (fp == NULL) {
+			perror("Problem kod brisanja datoteke");
+		}
+		fclose(fp);
+	}
+	system("cls");
 }
 
 void kalendar() {
