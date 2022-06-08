249d3caafc4a977601b95e256c025b3883296a1e
kdanilovic <105845601+kdanilovic@users.noreply.github.com>
2022-06-08 08:41:01 +0200

    Add files via upload

--- funkcije.h
+++ funkcije.h
@@ -1,7 +1,26 @@
 #ifndef FUNKCIJE_H
 #define FUNKCIJE_H
 
-	
-	
+typedef struct obaveze {
+	int br;
+	char obaveza[200];
+}OBAVEZE;
+
+static char* datoteka = "to do list.txt";
+int BrObaveze;
+
+void naslov();
+int izbornik();
+void stvori_datoteku();
+int BrojacDat();
+void dodaj();
+void prikaz();
+void IzbrisiDatoteku();
+OBAVEZE* zauzimanje(int);
+void UnosPolje(OBAVEZE*, int);
+void IzbrisiObavezu(OBAVEZE*, int);
+void UrediObavezu(OBAVEZE*, int broj);
+void kalendar();
+
 
 #endif //FUNKCIJE_H
